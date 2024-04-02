92cb413b2977c9c2e83199b877b13eef33bccc7d
kanth vagale <100186878+kanthvagale@users.noreply.github.com>
2024-04-02 16:08:16 +0530

    Add files via upload

---
+++ worldcup/insert_data.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+TRUNCATE=$($PSQL "TRUNCATE teams, games")
+
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != 'year' ]]
+  then
+    # add team to teams if not present
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    if [[ -z $WINNER_ID ]] 
+    then
+      echo "$($PSQL "INSERT INTO teams (name) VALUES ('$WINNER')")"
+      # GET WINNER_ID
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $OPPONENT_ID ]] 
+    then
+      echo "$($PSQL "INSERT INTO teams (name) VALUES ('$OPPONENT')")"
+      # GET OPPONENT_ID
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+    
+    # adding games into games table...
+    GAME_ID=$($PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    echo $GAME_ID
+  fi
+done  
+
+echo "$($PSQL "SELECT COUNT(*) FROM teams")"
+echo "$($PSQL "SELECT COUNT(*) FROM games")"
